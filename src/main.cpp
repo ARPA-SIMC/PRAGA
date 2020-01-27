6590d145a3f7dcca6c56e8057515a79d276f7f78
gantolini <gantolini@arpae.it>
2020-01-27 18:01:20 +0100

    return codes

--- src/main.cpp
+++ src/main.cpp
@@ -76,16 +76,12 @@ int main(int argc, char *argv[])
     }
     else if (myProject.modality == MODE_CONSOLE)
     {
-        if (pragaShell(&myProject))
-            return 0;
-        else
-            return 1;
+        if (! pragaShell(&myProject))
+            return -1;
     }
     else if (myProject.modality == MODE_BATCH)
     {
-        if (pragaBatch(&myProject, argv[1]))
-            return 0;
-        else
-            return 1;
+        if (! pragaBatch(&myProject, argv[1]))
+            return -1;
     }
 }
