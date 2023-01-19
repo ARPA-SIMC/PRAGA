6a15b54d9f880c1b72ee05ab813843e517355d0d
ftomei <ftomei@arpae.it>
2023-01-19 19:47:03 +0100

    update

--- src/mainWindow.cpp
+++ src/mainWindow.cpp
@@ -237,7 +237,7 @@ void MainWindow::resizeEvent(QResizeEvent *event)
 {
     Q_UNUSED(event)
 
-    ui->widgetMap->setGeometry(ui->widgetMap->x(), 0, this->width() - ui->widgetMap->x(), this->height() - 42);
+    ui->widgetMap->setGeometry(ui->widgetMap->x(), 0, this->width() - ui->widgetMap->x(), this->height() - 45);
     mapView->resize(ui->widgetMap->size());
 }
 
