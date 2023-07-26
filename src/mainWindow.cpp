6ba3101d107c6789341f074118fd9564198a0085
Gabriele Antolini <gantolini@arpae.it>
2023-07-26 16:16:23 +0200

    fixed typo

--- src/mainWindow.cpp
+++ src/mainWindow.cpp
@@ -816,8 +816,9 @@ void MainWindow::interpolateDemGUI()
 
 void MainWindow::interpolateGridGUI()
 {
-    if (myProject.interpolationMeteoGrid(myProject.getCurrentVariable(), myProject.getCurrentFrequency(),
-                                         myProject.getCrit3DCurrentTime()))
+    //if (myProject.interpolationMeteoGrid(myProject.getCurrentVariable(), myProject.getCurrentFrequency(),
+    //                                         myProject.getCrit3DCurrentTime()))
+    if (myProject.interpolationGridMain(myProject.getCurrentVariable(), myProject.getCrit3DCurrentTime()))
     {
         redrawMeteoGrid(showCurrentVariable, true);
     }
