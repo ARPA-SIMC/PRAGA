0d1a88122adeffd648e7808a4d4a25f66b4c0abc
Caterina Toscano <caterina.t98@gmail.com>
2024-03-29 11:10:44 +0100

    added local proxy graphs

--- src/mainWindow.cpp
+++ src/mainWindow.cpp
@@ -226,21 +226,16 @@ void MainWindow::mouseMove(const QPoint& mapPos)
     }
 
     Position geoPos = this->mapView->mapToScene(mapPos);
-    QString status = "Lat:" + QString::number(geoPos.latitude());
-    status += " Lon:" + QString::number(geoPos.longitude());
-
-    double x, y;
-    gis::latLonToUtmForceZone(myProject.gisSettings.utmZone, geoPos.latitude(), geoPos.longitude(), &x, &y);
-    status += " - UTM x:" + QString::number(x);
-    status += " y:" + QString::number(y);
+    QString status = "Lat:" + QString::number(geoPos.latitude())
+                   + " - Lon:" + QString::number(geoPos.longitude());
 
     // raster
     float value = NODATA;
     if (rasterObj->isLoaded && rasterObj->visible())
     {
         value = rasterObj->getValue(geoPos);
-        if (! isEqual(value, NODATA))
-            status += " - Raster value: " + QString::number(double(value),'f',1);
+        if (!isEqual(value, NODATA))
+            status += " - Raster: " + QString::number(double(value),'f',1);
     }
 
     // meteoGrid
@@ -286,14 +281,10 @@ void MainWindow::mouseMove(const QPoint& mapPos)
                 }
             }
 
-            status += " - Grid cell:" + QString::fromStdString(id);
-            if (name != id)
-            {
-                status += " " + QString::fromStdString(name);
-            }
+            status += " - Grid cell:" + QString::fromStdString(id + " " + name);
             if (!isEqual(value, NODATA))
             {
-                status += " Grid value: " + QString::number(double(value), 'f', 2);
+                status += " Value: " + QString::number(double(value), 'f', 2);
             }
         }
     }
