8bb0d23a080ad6ccb47654bc033fcd6b4b192ae9
ftomei <ftomei@arpae.it>
2025-01-07 17:10:45 +0100

    fix warnings

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "pragaProject.h"
 #include "pragaShell.h"
+#include "shell.h"
 #include "mainGUI.h"
 #include "commonConstants.h"
+
 #include <QCoreApplication>
 #include <iostream>
 #include <QtNetwork/QNetworkProxy>
